3e627e67974402221d1a24409335926abea9e15f
Sonya Huang <shuang@zushealth.com>
2024-04-07 17:45:11 -0400

    fix: do not drop dynamic tables when query changes
    Previously, there was a `or_replace` attribute on the
    snowflake_dynamic_table resource that was meant to specify whether
    `OR REPLACE` is meant to be used if the table already exists. However,
    this attribute was confusing to use because it often resulted in
    spurious changes in terraform plans after applying.
    
    A separate issue with dynamic tables was that we have been dropping and
    recreating dynamic tables when the `query` or `refresh_mode` properties
    changed, when we could take a less destructive action by calling `CREATE
    OR REPLACE`. The presence of `or_replace` as a configured attribute made
    this more complicated.
    
    This commit removes the ability for users to set `or_replace` on dynamic
    table configurations, and instead will always use `CREATE OR REPLACE`
    for dynamic table creation. With this simplification, we can then check
    for changes to the `query` and `refresh_mode` arguments when updating
    the dynamic table, and either user `CREATE OR REPLACE` or ALTER DYNAMIC
    TABLE` depending on which attributes have changed. This allows users to
    update the query and refresh mode without causing temporary
    unavailability.

--- pkg/sdk/testint/dynamic_table_integration_test.go
+++ pkg/sdk/testint/dynamic_table_integration_test.go
@@ -25,7 +25,7 @@ func TestInt_DynamicTableCreateAndDrop(t *testing.T) {
 		}
 		query := "select id from " + tableTest.ID().FullyQualifiedName()
 		comment := random.Comment()
-		err := client.DynamicTables.Create(ctx, sdk.NewCreateDynamicTableRequest(name, testClientHelper().Ids.WarehouseId(), targetLag, query).WithOrReplace(true).WithComment(&comment))
+		err := client.DynamicTables.Create(ctx, sdk.NewCreateDynamicTableRequest(name, testClientHelper().Ids.WarehouseId(), targetLag, query).WithComment(&comment))
 		require.NoError(t, err)
 		t.Cleanup(func() {
 			err = client.DynamicTables.Drop(ctx, sdk.NewDropDynamicTableRequest(name))
@@ -56,7 +56,7 @@ func TestInt_DynamicTableCreateAndDrop(t *testing.T) {
 		}
 		query := "select id from " + tableTest.ID().FullyQualifiedName()
 		comment := random.Comment()
-		err := client.DynamicTables.Create(ctx, sdk.NewCreateDynamicTableRequest(id, testClientHelper().Ids.WarehouseId(), targetLag, query).WithOrReplace(true).WithComment(&comment))
+		err := client.DynamicTables.Create(ctx, sdk.NewCreateDynamicTableRequest(id, testClientHelper().Ids.WarehouseId(), targetLag, query).WithComment(&comment))
 		require.NoError(t, err)
 		t.Cleanup(func() {
 			err = client.DynamicTables.Drop(ctx, sdk.NewDropDynamicTableRequest(id))
@@ -84,7 +84,7 @@ func TestInt_DynamicTableCreateAndDrop(t *testing.T) {
 		comment := random.Comment()
 		refreshMode := sdk.DynamicTableRefreshModeFull
 		initialize := sdk.DynamicTableInitializeOnSchedule
-		err := client.DynamicTables.Create(ctx, sdk.NewCreateDynamicTableRequest(id, testClientHelper().Ids.WarehouseId(), targetLag, query).WithOrReplace(true).WithInitialize(initialize).WithRefreshMode(refreshMode).WithComment(&comment))
+		err := client.DynamicTables.Create(ctx, sdk.NewCreateDynamicTableRequest(id, testClientHelper().Ids.WarehouseId(), targetLag, query).WithInitialize(initialize).WithRefreshMode(refreshMode).WithComment(&comment))
 		require.NoError(t, err)
 		t.Cleanup(func() {
 			err = client.DynamicTables.Drop(ctx, sdk.NewDropDynamicTableRequest(id))
@@ -221,7 +221,7 @@ func TestInt_DynamicTablesShowByID(t *testing.T) {
 			MaximumDuration: sdk.String("2 minutes"),
 		}
 		query := "select id from " + tableTest.ID().FullyQualifiedName()
-		err := client.DynamicTables.Create(ctx, sdk.NewCreateDynamicTableRequest(id, warehouseId, targetLag, query).WithOrReplace(true))
+		err := client.DynamicTables.Create(ctx, sdk.NewCreateDynamicTableRequest(id, warehouseTest.ID(), targetLag, query).WithOrReplace(true))
 		require.NoError(t, err)
 		t.Cleanup(cleanupDynamicTableHandle(t, id))
 	}
